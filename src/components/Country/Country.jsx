9e6f528af5512dd5108046ceaca048936c8f2c42
Hasan Al Mahmud <noiraxen@proton.me>
2025-10-06 21:18:28 +0600

    added conditional css class and values

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({ country }) => {
+    const [visited, setVisited] = useState(false)
     // console.log(country.area.area);
     // console.log(Object.values(country.currencies.currencies));
     // console.log(Object.values(country.currencies.currencies)[0].name);
 
-    const handleVisited = ()=> {
-        console.log('button clicked');
-        
+    const handleVisited = () => {
+        // basic system
+        // if (visited) {
+        //     setVisited(false)
+        // } else {
+        //     setVisited(true)
+        // }
+
+        // second system
+        // handleVisited(visited ? false : true);
+
+        // third system
+        setVisited(!visited)
+
     }
-    
+
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Official name: {country.name.official}</p>
             <p>Population: {country.population.population}</p>
             <p>Capital: {country.capital.capital.join(', ')}</p>
             <p>Languages: {Object.values(country.languages.languages).join(', ')}</p>
-            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country": "Small Country"}</p>
-            <button onClick={handleVisited}>Not Visited</button>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
+            <button onClick={handleVisited}>
+                {visited ? "Visited" : "Not Visited"}
+            </button>
         </div>
     );
 };
