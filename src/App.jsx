73051b30ba3047a413010924f414243a8c09d0b0
Hasan Al Mahmud <noiraxen@proton.me>
2025-10-06 18:04:12 +0600

    added react template

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Country from './components/countries/Country'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Country></Country>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/countries/Country.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Country = () => {
+    return (
+        <div>
+            <h1>In the Country.</h1>
+        </div>
+    );
+};
+
+export default Country;
