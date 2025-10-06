49b1d7635d15fd298ae70505c654e1a39510dd26
Hasan Al Mahmud <noiraxen@proton.me>
2025-10-06 20:12:43 +0600

    added country details

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Country = () => {
+const Country = ({country}) => {
+    console.log(country);
+    // console.log(Object.values(country.currencies.currencies));
+    // console.log(Object.values(country.currencies.currencies)[0].name);
+    
     return (
         <div>
-            <h3>Name: </h3>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common}</h3>
+            <p>Official name: {country.name.official}</p>
+            <p>Population: {country.population.population}</p>
+            <p>Capital: {country.capital.capital.join(', ')}</p>
+            <p>Languages: {Object.values(country.languages.languages).join(', ')}</p>
         </div>
     );
 };
