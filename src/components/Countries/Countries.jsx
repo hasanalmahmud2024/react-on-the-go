c4459957361e41c6f5ddb1e333807b22e4e811eb
Hasan Al Mahmud <noiraxen@proton.me>
2025-10-07 22:29:02 +0600

    lifted up the state to show the visited flags

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,37 +1,52 @@
-import React, { use, useState } from 'react';
-import Country from '../Country/Country';
-import './Countries.css'
+import React, { use, useState } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = ({ countriesPromise }) => {
-    const [visitedCountries, setVisitedCountries] = useState([])
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const [visitedFlags, setVisitedFlags] = useState([]);
 
-    const handleVisitedCountries = (country) => {
-        // console.log("country visited", country);
-        const newVisitedCountries = [...visitedCountries, country]
-        setVisitedCountries(newVisitedCountries)
+  const handleVisitedCountries = (country) => {
+    // console.log("country visited", country);
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
+  const handleVisitedFlags = (flag) => {
+    const newVisitedFlags = [...visitedFlags, flag];
+    setVisitedFlags(newVisitedFlags);
+  };
 
-    }
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
 
-    const countriesData = use(countriesPromise);
-    const countries = countriesData.countries;
+  return (
+    <div>
+      <h1>In the Countries: {countries.length}</h1>
+      <h3>Total Country Visited: {visitedCountries.length}</h3>
+      <h3>Total Flags Visited: {visitedFlags.length}</h3>
+      <ol>
+        {visitedCountries.map((country) => (
+          <li key={country.ccn3.ccn3}>{country.name.common}</li>
+        ))}
+      </ol>
+      <div className="visited-flags-container">
+        {visitedFlags.map((flag, index) => (
+          <img key={index} src={flag}></img>
+        ))}
+      </div>
 
-
-    return (
-        <div >
-            <h1>In the Countries: {countries.length}</h1>
-            <h3>Total Country Visited: {visitedCountries.length}</h3>
-            <ol>
-                {
-                    visitedCountries.map(country => <li key={country.cca3.cca3}>{country.name.common}</li>)
-                }
-            </ol>
-            <div className="countries">
-                {
-                    countries.map(country => <Country key={country.cca3.cca3} country={country} handleVisitedCountries={handleVisitedCountries} ></Country>)
-                }
-            </div>
-        </div>
-    );
+      <div className="countries">
+        {countries.map((country) => (
+          <Country
+            key={country.cca3.cca3}
+            country={country}
+            handleVisitedCountries={handleVisitedCountries}
+            handleVisitedFlags={handleVisitedFlags}
+          ></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,50 +1,44 @@
-import React, { useState } from 'react';
-import './Country.css'
+import React, { useState } from "react";
+import "./Country.css";
 
-const Country = ({ country, handleVisitedCountries }) => {
-    const [visited, setVisited] = useState(false)
-    // console.log(country.area.area);
-    // console.log(Object.values(country.currencies.currencies));
-    // console.log(Object.values(country.currencies.currencies)[0].name);
-    // console.log(handleVisitedCountries)
-    const handleVisited = () => {
-        // basic system
-        // if (visited) {
-        //     setVisited(false)
-        // } else {
-        //     setVisited(true)
-        // }
+const Country = ({ country, handleVisitedCountries, handleVisitedFlags }) => {
+  const [visited, setVisited] = useState(false);
+  // console.log(Object.values(country.currencies.currencies)[0].name);
+  const handleVisited = () => {
+    // basic system : ifelse
+    // if (visited) {setVisited(false)} else {setVisited(true)}
+    // second system : ternary operator
+    // handleVisited(visited ? false : true);
+    // third system
+    setVisited(!visited);
+    handleVisitedCountries(country);
+  };
 
-        // second system
-        // handleVisited(visited ? false : true);
-
-        // third system
-        setVisited(!visited)
-        handleVisitedCountries(country)
-    }
-
-    return (
-        <div className={`country ${visited && 'country-visited'}`}>
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-            <h3>Name: {country.name.common}</h3>
-            <p>Official name: {country.name.official}</p>
-            <p>Population: {country.population.population}</p>
-            <p>Capital: {country.capital.capital.join(', ')}</p>
-            <p>Languages: {Object.values(country.languages.languages).join(', ')}</p>
-            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
-            <button onClick={handleVisited}>
-                {visited ? "Visited" : "Not Visited"}
-            </button>
-            <button>Add Visited Flag</button>
-        </div>
-    );
+  return (
+    <div className={`country ${visited && "country-visited"}`}>
+      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+      <h3>Name: {country.name.common}</h3>
+      <p>Official name: {country.name.official}</p>
+      <p>Population: {country.population.population}</p>
+      <p>Capital: {country.capital.capital.join(", ")}</p>
+      <p>Languages: {Object.values(country.languages.languages).join(", ")}</p>
+      <p>
+        Area: {country.area.area}{" "}
+        {country.area.area > 300000 ? "Big Country" : "Small Country"}
+      </p>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
+      <button onClick={()=>{handleVisitedFlags(country.flags.flags.png)}}>Add Visited Flag</button>
+    </div>
+  );
 };
 
 export default Country;
 
-
 /**
+ * how to add CSS
  * 1. inline css (style object)
  * 2. import from file
- * 3.
+ * 3. tailwind
  */
